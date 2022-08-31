060fd2d41271758c1b8ba4ee27668464f082c167
DESKTOP-J210O8V\TibeMe_user <j6jo6z06@gmail.com>
2022-08-31 15:22:40 +0800

    add getPlayS

--- app.py
+++ app.py
@@ -202,6 +202,10 @@ def getCallCarMessage(data):
 
 def getPlayStickerMessage():
     message = dict()
+    message["type"] = "sticker"
+    message["packageId"] = "446"
+    message["stickerId"] = "1988"
+
     return message
 
 
